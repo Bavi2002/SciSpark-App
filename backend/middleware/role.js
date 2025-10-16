ac95275812157abba5861536c37711af3ecd88a2
Dilakshan Pahitharan <it22297372@my.sliit.lk>
2025-10-16 09:48:14 +0530

    authorize update

--- backend/middleware/role.js
+++ backend/middleware/role.js
@@ -1,1 +1,17 @@
 //Role based acces
+
+// src/middleware/roleAuth.js
+const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res
+        .status(403)
+        .json({
+          message: `User role ${req.user.role} is not authorized for this action`,
+        });
+    }
+    next();
+  };
+};
+
+export default authorize;
